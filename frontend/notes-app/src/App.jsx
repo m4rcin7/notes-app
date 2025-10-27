99d21c88711cf414c39f60958d3ce848460c608c
Marcin Kopatys <mrku1337@gmail.com>
2025-10-27 14:44:23 +0100

    login-form

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,7 +1,8 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 const routes = (
--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
 
 export default function Login() {
   return (
     <>
       <NavBar />
+
+      <div>
+        <form onSubmit={() => {}}>
+          <h4>Login</h4>
+          <input type="text" placeholder="Email" className="input-box" />
+          <button type="submit" className="btn-primary">
+            Login
+          </button>
+
+          <p>
+            Not registered yet? <Link to="/signup">Create an Account</Link>
+          </p>
+        </form>
+      </div>
     </>
   );
 }
