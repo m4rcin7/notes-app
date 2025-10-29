c1e3ecb35dff687e75c5ea36f87e210aea936347
Marcin Kopatys <mrku1337@gmail.com>
2025-10-29 18:39:23 +0100

    fixing bugs

--- frontend/notes-app/src/components/NavBar/NavBar.jsx
+++ frontend/notes-app/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,9 @@
 export default function NavBar() {
   return (
-    <nav className="flex items-center justify-between px-6 py-3 bg-neutral-900 text-white">
-      <h2 className="text-lg font-semibold tracking-wide">Notes</h2>
+    <nav className="fixed top-0 left-0 w-full bg-neutral-900 text-white px-6 py-4 shadow-lg z-50">
+      <div className="flex items-center justify-between max-w-7xl mx-auto">
+        <h2 className="text-lg font-semibold tracking-wide">Notes</h2>
+      </div>
     </nav>
   );
 }
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,3 +1,9 @@
+import NavBar from "../../components/NavBar/NavBar";
+
 export default function Home() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
