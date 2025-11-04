9118452e0db2c7d112ef5fb825189a263df6f31d
Marcin Kopatys <mrku1337@gmail.com>
2025-11-04 20:04:30 +0100

    profile-info-

--- frontend/notes-app/src/components/NavBar/NavBar.jsx
+++ frontend/notes-app/src/components/NavBar/NavBar.jsx
@@ -3,6 +3,8 @@ export default function NavBar() {
     <nav className="fixed top-0 left-0 w-full bg-neutral-900 text-white px-6 py-4 shadow-lg z-50">
       <div className="flex items-center justify-between max-w-7xl mx-auto">
         <h2 className="text-lg font-semibold tracking-wide">Notes</h2>
+
+        <ProfileInfo />
       </div>
     </nav>
   );
